fda51659bfb78de809dfca97dc3faf8a7f5f2b29
MclPio <113801014+MclPio@users.noreply.github.com>
2023-05-03 16:03:35 -0400

    add tests for player1_turn, player2_turn, and player_input

--- lib/game.rb
+++ lib/game.rb
@@ -69,5 +69,5 @@ class Game
   end
 end
 
-game = Game.new
-game.play_game
+# game = Game.new
+# game.play_game
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -22,10 +22,42 @@ describe Game do
     end
   end
 
+  describe 'player1_turn' do
+    subject (:game) { described_class.new }
+
+    context 'when column 6 is choosen' do
+      before do
+        allow(game).to receive(:gets).and_return('6')
+        allow(game.connect_four).to receive(:puts)
+        allow(game).to receive(:puts)
+        game.player1_turn
+      end
+      it 'updates game [5][5] to 🔴' do
+        expect(game.connect_four.board[5][5]).to eq('🔴')
+      end
+    end
+  end
+
+  describe 'player2_turn' do
+    subject (:game) { described_class.new }
+
+    context 'when column 7 is choosen' do
+      before do
+        allow(game).to receive(:gets).and_return('7')
+        allow(game.connect_four).to receive(:puts)
+        allow(game).to receive(:puts)
+        game.player2_turn
+      end
+      it 'updates board [5][6] to 🟡' do
+        expect(game.connect_four.board[5][6]).to eq('🟡')
+      end
+    end
+  end
+
   describe 'row_full?' do
     subject(:game) { described_class.new }
 
-    context 'when row is full' do
+    context 'when 1 row is full' do
       it 'returns true' do
         6.times do
           game.connect_four.insert_piece(1, 'X')
@@ -34,7 +66,7 @@ describe Game do
       end
     end
 
-    context 'when row is empty' do
+    context 'when row 1 is empty' do
       it 'returns false' do
         expect(game.row_full?(1)).to eq(false)
       end
@@ -42,8 +74,30 @@ describe Game do
   end
 
   describe '#player_input' do
-    context 'should group actions of clean_input and row_full?' do
-      xit 'will have to return valid input'
+    subject(:game) { described_class.new }
+
+    context 'when column 6 is choosen but is full' do
+      before do
+        allow(game).to receive(:gets).and_return('6')
+        allow(game.connect_four).to receive(:puts)
+        allow(game).to receive(:row_full?).and_return(true, false)
+      end
+      
+      it 'returns "column is full, choose another!"' do
+        expect{ game.player_input }.to output("column is full, choose another!\n").to_stdout
+      end
+    end
+
+    context 'when column 7 is choosen and is not full' do
+      before do
+        allow(game).to receive(:gets).and_return('7')
+        allow(game.connect_four).to receive(:puts)
+        allow(game).to receive(:row_full?).and_return(false)
+      end
+      
+      it 'returns column 7 back' do
+        expect(game.player_input).to eq(7)
+      end
     end
   end
 end
